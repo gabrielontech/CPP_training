7948be90549333cccffb3dc088035a00cf3e59c3
Nucleon <70663867+kellerM42@users.noreply.github.com>
2023-02-01 23:36:48 +0000

    fix(CPP01/ex02): :sparkles: changed the text in english

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -5,11 +5,11 @@ int main()
     std:: string str("HI THIS IS BRAIN");
     std:: string *stringPTR = &str;
     std:: string &stringREF = str;
-    
-    std::cout << "Adresse de str en mémoire : "  << &str << std::endl;
-    std::cout << "Adresse stockée dans stringPTR : "  <<stringPTR << std::endl;
-    std::cout << "Adresse stockée dans stringREF : " << &stringREF << std::endl;
-    std::cout << "Valeur de str : " << str << std::endl;
-    std::cout << "Valeur pointée par stringPTR: "  << *stringPTR << std::endl;
-    std::cout << "Valeur pointée par stringREF: " << stringREF << std::endl;
+
+    std::cout << "Adress of str in the memory : "  << &str << std::endl;
+    std::cout << "Stored adress in stringPTR : "  <<stringPTR << std::endl;
+    std::cout << "Stored adress in stringREF : " << &stringREF << std::endl;
+    std::cout << "Value of str : " << str << std::endl;
+    std::cout << "Value pointed by stringPTR: "  << *stringPTR << std::endl;
+    std::cout << "Value pointed by stringREF: " << stringREF << std::endl;
 }
