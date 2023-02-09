655557030be0dacdffe30c880b6c1da46d3a0586
Nucleon <70663867+kellerM42@users.noreply.github.com>
2023-02-09 15:20:54 +0000

    feat(CPP02/includes): :sparkles: done with the .hpp

---
+++ CPP02/ex00/includes/Fixed.hpp
@@ -0,0 +1,23 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <iostream>
+# include <string>
+
+class Fixed
+{
+    private:
+        int _value;
+        const int _bits = 8;
+    public:
+        //constructors and destructors
+        Fixed();
+        Fixed(int value);
+        int     getRawBits( void )const;
+        void    setRawBits( int const raw );
+        Fixed(const Fixed& other);
+        ~Fixed();
+        //overloads
+        Fixed& operator=(const Fixed& rhs);
+};
+
+#endif
